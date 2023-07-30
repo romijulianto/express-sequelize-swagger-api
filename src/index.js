70d5f27ae61238d4422cd90ba483c5df2024fd24
Romi Julianto <romyjulians@gmail.com>
2023-07-30 18:59:07 +0700

    chore: setup deploy railway

--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  const baseUrl = "https://media-backend.up.railway.app";
+  res.redirect(`${baseUrl}/api/docs`);
+});
+
 // add route for swagger document API
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/users", userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+
+app.listen(process.env.PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port`);
 });
