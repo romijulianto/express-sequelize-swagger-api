7832bba2fcbe92bf153aedac4ca25e80261a39f0
Romi Julianto <romyjulians@gmail.com>
2023-11-21 10:51:55 +0700

    fix: client password must be string

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
+  `${process.env.DATABASE_PASSWORD}`,
   {
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
